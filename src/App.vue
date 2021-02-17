3b1b96a4b724d2dc0c0ad32dc0ed03a3bc72a33b
lvfeng <353823561@qq.com>
2021-02-17 18:38:36 +0800

    add tabbar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
 
+<script>
+// import MainTabBar from 'components/content/MainConponents/MainTabBar'
+export default {
+  name: 'app',
+  components: {
+
+  }
+}
+</script>
+
 <style>
+  @import 'assets/css/base.css';
 </style>
---
+++ vue.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+function resolve(dir) {
+   return path.join(__dirname, dir)
+}
+module.exports = {
+   chainWebpack: (config)=>{
+      config.resolve.alias
+         .set('@', resolve('src'))
+         .set('assets', resolve('src/assets'))
+         .set('components', resolve('src/components'))
+         .set('views', resolve('src/views'))
+         .set('routers', resolve('src/router'))
+   }
+}
+
+// 方式二：
+// module.exports = {
+//    configureWebpack: {
+//       resolve: {
+//          alias: {
+//             'assets': 'src/assets',
+//             'components': 'src/components',
+//             'views': 'src/views',
+//             'routers': 'src/router'
+//          }
+//       }
+//    }
+// }
